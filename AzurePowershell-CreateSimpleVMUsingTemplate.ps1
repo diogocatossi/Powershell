e1dd74c7fc0e79ee9a9b9254fd676183dabbd820
Diogo Catossi <48570909+diogocatossi@users.noreply.github.com>
2020-06-10 12:56:58 -0300

    Azure connection validation
    Added extra Azure context validation

--- AzurePowershell-CreateSimpleVMUsingTemplate.ps1
+++ AzurePowershell-CreateSimpleVMUsingTemplate.ps1
@@ -2,8 +2,7 @@
     .SYNOPSIS
         Create and Azure VM using specified template.
     .DESCRIPTION
-        Create and Azure VM using specified template and validating errors in case of failure, providing the specific error message.
-	The desired template URI should be provided as parameter
+        Create and Azure VM using specified template and validating errors in case of failure, providing the specific error message of which deployment step failed. The desired template URI should be provided as parameter
     .INPUTS
         ResourceGroupName - Name of the resource group that will be used for the VM deployment. If none exist a new one will be created.
 		TemplateURI - URI for the template desired. e.g.: 'https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-windows/azuredeploy.json' (used for this template initially)
@@ -30,10 +29,42 @@ param
 
 Import-Module Az
 
+function isURI($address) {
+	($address -as [System.URI]).AbsoluteURI -ne $null
+}
 
-#Variables
+function isURIWeb($address) {
+	$uri = $address -as [System.URI]
+	$uri.AbsoluteURI -ne $null -and $uri.Scheme -match '[http|https]'
+}
+
+
+###### Variables #######
 $deploymentName = "$ResourceGroupName-Deployment-$(get-date -Format yyyyMMdd-HHmm)"
+$tab0 = "`t" * 0
+$tab1 = "`t" * 1
+$tab2 = "`t" * 2
+$tab3 = "`t" * 3
 
+###### Validation ######
+if (-Not (isURI($TemplateURI))) {
+    write-host "$tab2 Please provide a valid URI for the template."
+    return
+}
+
+
+# Validates there's a valid Azure connection for the current profile
+if (-not (Get-AzContext)){
+    Write-Host "$tab2 There's no active Azure Subscription context. Starting authentication process..."
+    Connect-AzAccount
+    if (Get-AzContext) {
+        write-host "$tab1 Azure connection successful. Proceeding."
+    }
+}
+
+
+
+## Verifies if provided RG exists
 try
 {
 	Get-AzResourceGroup -Name $ResourceGroupName -ErrorAction Stop
@@ -41,14 +72,18 @@ try
 }
 catch
 {
+	## If it doesn't exists will attempt to create it
 	$list = @()
+	
+	#Retrieves all curretly available locations
 	$locations = Get-AzLocation | Select-Object -Property Location
 	foreach ($l in $locations) { $list += "`t$($l.location)`n`r" }
 	
 	Write-Host "The resource group name provided doesn't exist. A new one must be created. `nFollows the list of the $($locations.count) available Locations: `n$list"
 	
-	$location = Read-Host -Prompt "Provide location for the Resource creation"
+	$location = Read-Host -Prompt "Provide location for the resource creation"
 	
+	#Attempts to create the RG
 	$rg = New-AzResourceGroup -Name $ResourceGroupName -Location $location
 	if (Get-AzResourceGroup -Name $ResourceGroupName)
 	{
@@ -62,6 +97,7 @@ catch
 	}
 }
 
+#Executes the remote command based on the parameters provided
 $result = New-AzResourceGroupDeployment -name $deploymentName -ResourceGroupName $ResourceGroupName -TemplateUri $TemplateURI
 
 if ($result.ProvisioningState -eq 'Succeeded')
@@ -82,4 +118,5 @@ else
 		}
 	}
 }
+
 
